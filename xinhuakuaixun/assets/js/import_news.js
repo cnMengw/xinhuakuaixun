8e13c94e041e09ad2d06d96249809b05e33fb51b
mw <747810741@qq.com>
2017-05-06 16:21:57 +0800

    误删的文件

---
+++ xinhuakuaixun/assets/js/import_news.js
@@ -0,0 +1,45 @@
+mui.init();
+			(function($){
+				var deceleration=mui.os.ios?0.003:0.0009;
+				$(".mui-scroll-wrapper").scroll({
+					bounce:false,
+					indicators:true,
+					deceleration:deceleration
+				});
+				
+				
+				$.ready(function(){
+					$.each(document.querySelectorAll(".news .mui-scroll"), function(index,pullRefreshE1) {
+						$(pullRefreshE1).pullToRefresh({
+							down:{
+								auto: false,
+								callback:function(){
+									var self = this;
+									setTimeout(function() {
+											
+											self.endPullDownToRefresh();
+										}, 1000);
+									
+								}
+							},
+							up:{
+								auto: false,
+								contentnomore: '没有更多数据了',
+								callback:function(){
+									var self=this;
+									setTimeout(function() {
+											
+											self.endPullUpToRefresh();
+										}, 1000);
+								}
+							}
+						})
+					});
+					
+				})
+			})(mui);
+	var sliderbanner = mui("#bannerslider");
+	sliderbanner.slider({
+		interval: 5000
+	});
+				
